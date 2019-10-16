436981303b78cee235bcf839e6afb7990dfbbbb9
Akihiro Nishikawa <akihiro.nishikawa@oracle.com>
2019-10-16 20:08:09 +0900

    Initial

---
+++ src/main/java/com/kvfuncref/Function.java
@@ -0,0 +1,32 @@
+package com.kvfuncref;
+
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+    /**
+     * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpTrigger-Java&code={your function key}
+     * 2. curl "{your host}/api/HttpTrigger-Java?name=HTTP%20Query&code={your function key}"
+     * Function Key is not needed when running locally, it is used to invoke function deployed to Azure.
+     * More details: https://aka.ms/functions_authorization_keys
+     */
+
+    @FunctionName("HttpTrigger-Java")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        return request.createResponseBuilder(HttpStatus.OK)
+                .body("SECRET1=" + System.getenv("KEYVAULT_SECRET1") + " / SECRET2=" + System.getenv("KEYVAULT_SECRET2")).build();
+
+    }
+}
